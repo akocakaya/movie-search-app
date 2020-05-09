86367dbbcf5a7de6da3c32d18bfa39fb44a3d9e3
Abdullah Kocakaya <abdullahkocakaya@Abdullahs-iMac.local>
2020-05-09 18:17:21 +0300

    env key fix

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ const App = () => {
     const [movies, setMovies] = useState([]);
 
     const search = searchValue => {
-        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.API_KEY}`)
+        fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}`)
             .then(response => response.json())
             .then(jsonResponse => {
                 console.log('jsonResponse -> ', jsonResponse)

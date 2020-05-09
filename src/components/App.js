711d88c91329786b60e0c035411282cfc408109a
Abdullah Kocakaya <abdullahkocakaya@Abdullahs-iMac.local>
2020-05-09 18:53:41 +0300

    error added

--- src/components/App.js
+++ src/components/App.js
@@ -7,9 +7,11 @@ import '../App.css';
 const App = () => {
     const [loading, setLoading] = useState(false);
     const [movies, setMovies] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const search = searchValue => {
         setLoading(true);
+        setErrorMessage(null);
 
         fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=${process.env.REACT_APP_API_KEY}`)
             .then(response => response.json())
@@ -19,28 +21,26 @@ const App = () => {
                     setMovies(jsonResponse.Search);
                     setLoading(false);
                 } else {
+                    setErrorMessage(jsonResponse.Error);
                     setLoading(false);
                 }
             });
     };
-      
-    const getMovies = () => {
-        if(loading)
-            return <span>Loading...</span>
-
-        return (
-            movies.map( ( movie, index ) => {
-                return <Movie key={'movie' + index} movie={ movie } />
-            })
-        );
-    }
 
     return (
         <div className="App">
             <Header text="Movie Search App" />
             <Search search={search} />
             <div className="movies">
-                { getMovies() }
+            {
+                loading && !errorMessage 
+                    ?   <span>loading...</span>
+                    :   errorMessage 
+                            ?   <div className="errorMessage">{errorMessage}</div> 
+                            :   movies.map((movie, index) => (
+                                    <Movie key={`${index}-${movie.Title}`} movie={movie} />
+                                ))
+            }
             </div>
         </div>
     );
